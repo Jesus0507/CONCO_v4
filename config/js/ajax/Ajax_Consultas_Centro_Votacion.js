7f455e73684764a8025a822caacc8a5f170fa3c9
Shuujin17 <79924041+Shuujin17@users.noreply.github.com>
2022-10-05 10:08:56 -0400

    cambio bracho

--- config/js/ajax/Ajax_Consultas_Centro_Votacion.js
+++ config/js/ajax/Ajax_Consultas_Centro_Votacion.js
@@ -115,7 +115,7 @@
                       peticion: "Consulta_Ajax",
                   },
               }).done(function(datos) {
-                alert(datos);
+                
                   var data = JSON.parse(datos);
                   var tabla = $("#example1").DataTable({
                       "data": data,
