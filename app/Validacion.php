28feb064ae2552eea9e83d855e11126de1f29452
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2022-11-22 18:57:17 -0400

    CLASE VALIDACION PRIVAIZADA

--- app/Validacion.php
+++ app/Validacion.php
@@ -1,19 +1,45 @@
 <?php
 
-class Validacion 
+class Validacion
 {
-    private $valido = false;
-    public  $mensaje;
+    private $pattern; #El patrón de búsqueda, dado como string
+    private $valor; #La cadena de entrada.
+    // patrones de búsqueda
+    private const CEDULA     = "/^([0-9]{7,9})$/";
+    private const CARACTERES = "/^[a-zA-Z0-9Ññáéíóú \b]{1,100}$/";
+    private const RIF        = "/^([vejpgVEJPG]{1})([0-9]{9})$/";
+    private const ENTEROS    = "/^([0-9]{1,2})$/";
+    private const DINERO     = "/^^\d{1,3}(?:\.\d\d\d)*(?:,\d{1,2})?$/";
+    private const BASE64     = "/^[a-zA-Z0-9\/\r\n+]*={0,2}$/";
+    private const PASSWORD   = "/(?=^.{4,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/";
+    private const TELEFONO   = "/^([0-9]{11,13})$/";
 
-    public function __construct(){}
-    
+    private const FECHAS = "/^(19|20)(((([02468][048])|([13579][26]))-02-29)|(\d{2})-((02-((0[1-9])|1\d|2[0-8]))|((((0[13456789])|1[012]))-((0[1-9])|((1|2)\d)|30))|(((0[13578])|(1[02]))-31)))$/";
+
+    private const CORREO = "/^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/";
+
+    // SETTER estaablece los datos a usar
+    public function __construct()
+    {}
+    public function Datos_Filtro($valor, $expression)
+    {
+        $this->pattern = $this->{$expression}; #variable anonima en espera de asignar nombre
+        $this->valor   = $valor; #valor de entrada para validar
+    }
+    // ===============================================================================
+    public function Validar()
+    {
+        #Realiza una comparación global de una expresión regular, retorna a true o false 
+        return (bool) (!preg_match_all($this->pattern, $this->valor)) ? true : false;
+    }
+
     public function Comprobar($value)
     {
         return (empty($value) && isset($value)) ? true : false;
     }
     public function Validar_Estado($value)
     {
-        if (!is_numeric($value) || $value !== 0  && $value !== 1 && $value !== 2) {return false;}else{return true;}
+        if (!is_numeric($value) || $value !== 0 && $value !== 1 && $value !== 2) {return false;} else {return true;}
     }
     public function Validar_Caracteres($valor)
     {
@@ -40,7 +66,8 @@ class Validacion
         return (!preg_match('/^(19|20)(((([02468][048])|([13579][26]))-02-29)|(\d{2})-((02-((0[1-9])|1\d|2[0-8]))|((((0[13456789])|1[012]))-((0[1-9])|((1|2)\d)|30))|(((0[13578])|(1[02]))-31)))$/', $fecha)) ? true : false;
     }
 
-    public function Verificar_Base64($value) { return (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $value)) ? true : false; }
+    public function Verificar_Base64($value)
+    {return (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $value)) ? true : false;}
 
     public function Validar_Contrasenia($valor)
     {
@@ -57,15 +84,6 @@ class Validacion
         return str_replace([' ', '.', '-', '(', ')'], '', $valor);
     }
 
-    //verifica que sea un email valido
-    public function Correo($nombre, $value)
-    {
-        if (filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            return $value;
-        }$this->valido = true;
-        $this->mensaje = 'El campo ' . $nombre . ' no tiene una dirección de correo electronico válida.';
-    }
-
     public function Datos_Limpios($data)
     {
         $data = trim($data);
@@ -75,9 +93,4 @@ class Validacion
         return $data;
     }
 
-    //devuelve validade para comprobar si hubo algun error en la validacion
-    public function Fallo_Validacion()
-    {
-        return $this->valido;
-    }
 }
