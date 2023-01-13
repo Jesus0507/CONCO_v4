372e7e29d83b15ccda4570c06faafff35fc84fec
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2023-01-13 11:18:50 -0400

    privatizando seguridad

--- controlador/seguridad_controlador.php
+++ controlador/seguridad_controlador.php
@@ -1,12 +1,92 @@
 <?php
-
+ 
 class Seguridad extends Controlador
 {
+    #Public: acceso sin restricción.
+    #Private:Solo puede ser accesado por la clase que lo define.
+    private $permisos;          #permisos correspondiente del modulo
+    private $peticion;          #peticion a ejecutar de la funcion administrar
+    private $estado;            #array con parametros de eliminacion logica (tabla,id_tabla,param,estado)
+    private $estado_ejecutar;   #array con parametro a ejecutar (id_tabla, estado)
+    private $sql;               #nombre de la sentencia SQL que se ejecutara en el modelo
+    private $datos_ejecutar;    #array con datos para enviar a la bd 
+    private $datos_consulta;    #array con los datos necesarios para el modulo (consultas)
+    private $accion;            #accion para enviar a la bitacora 
+    private $mensaje;           #mensaje que se mandara a la vista
+    private $validar;           #objeto con la clase validacion correspondiente al modulo
+    private $crud;              #array con peticion generica sql
+
+    // DATOS independientes usados para el manejo del modulo
+
+    // ==================ESTABLECER DATOS=========================
     public function __construct()
     {
         parent::__construct();
         // $this->Cargar_Modelo("seguridad");
     }
+
+    // ==================GETTERS=========================
+    #getters usados para obtener la informacion de las variables privadas
+    # retornan tipo string o array
+    private function Get_Sql():string           {return $this->sql;}
+    private function Get_Accion():string        {return $this->accion;}
+    private function Get_Mensaje():string       {return $this->mensaje;}
+    private function Get_Datos():array          {return $this->datos_ejecutar;}
+    private function Get_Estado():array         {return $this->estado;}
+    private function Get_Estado_Ejecutar():array{return $this->estado_ejecutar;}
+    private function Get_Datos_Vista():array    {return $this->datos_consulta;}
+    private function Get_Crud_Sql(): array      {return $this->crud;}
+    // ==============================================================================
+
+    public function Administrar($peticion = null)
+    {
+        $this->Seguridad_de_Session();
+        $this->Establecer_Consultas();
+        $this->peticion = (isset($_POST['peticion'])) ? $_POST['peticion'] : $peticion[0];
+        switch ($this->peticion) {
+            case 'Consultas':
+                if ($this->permisos["consultar"] === 1) {
+                    $this->vista->Cargar_Vistas('negocios/consultar');
+                } else { $this->_403_();}
+                break;
+            case 'Administrar':
+                if ($this->permisos["registrar"] === 1 || $this->permisos["modificar"] === 1) {
+
+                    if ($this->validar->Validacion_Registro()) {
+                        $this->modelo->_Datos_($this->Get_Datos());
+                        $this->modelo->_SQL_($this->Get_Sql());
+                        $this->modelo->_Tipo_(1);
+                        if ($this->modelo->Administrar()) {
+                            $this->Accion($this->Get_Accion());
+                            echo $this->Get_Mensaje();
+                        }
+                    } else {
+                        echo $this->validar->Fallo();
+                    }
+                } else { $this->_403_();}
+                break;
+            case 'Eliminar':
+                if ($this->permisos["eliminar"] === 1) {
+                    $this->modelo->_Estado_($this->Get_Estado());
+                    $this->modelo->_Datos_($this->Get_Estado_Ejecutar());
+                    $this->modelo->_SQL_($this->Get_Sql());
+                    $this->modelo->_Tipo_(1);
+                    if ($this->modelo->Administrar()) {
+                        $this->Accion($this->Get_Accion());
+                        echo $this->Get_Mensaje();
+                    }
+                } else { $this->_403_();}
+                break;
+
+
+            default:$this->vista->Cargar_Vistas('error/400');
+                break;
+        }
+        exit();
+        
+    }
+
+    // ==============================================================================
 
     public function Cargar_Vistas()
     {
--- modelo/seguridad_class.php
+++ modelo/seguridad_class.php
@@ -1,8 +1,88 @@
 <?php
 
 class Seguridad_Class extends Modelo
-{
+{ 
+    #Public: acceso sin restricción.
+    #Private:Solo puede ser accesado por la clase que lo define.
+
+    private $SQL;           #nombre de la sentencia SQL que se ejecutara en el modelo
+    private $tipo;          #tipo de peticion que usaremos 1/0
+    private $PDO;           #sentecia sql iniciada con prepare
+    private $sentencia;     #sentencia sql que se ejecutara
+    private $datos;         #datos a ejecutar para enviar a la bd
+
+    public  $resultado;     #resultado de consultas de la bd
+
     public function __construct(){parent::__construct();}
+
+    // SETTER estaablece los datos a usar en el modelo (tipo void no retornan un valor)
+    public function _SQL_(string $SQL): void        {$this->SQL = $SQL;}
+    public function _Tipo_(int $tipo): void         {$this->tipo = $tipo;}
+    public function _Datos_(array $datos): void     {$this->datos = $datos;}
+    public function _Estado_(array $estado): void   {$this->estado = $estado;}
+
+    public function Administrar()
+    {
+        $this->sentencia = $this->{$this->SQL}(); #funcion anonima en espera de asignar nombre
+        try {
+            switch ($this->tipo) {
+                case '0':           #tipo 0 trae consultas de la bd retorna a un array con los datos 
+                    $this->conexion->beginTransaction(); # Inicia una transacción
+                    $this->PDO = $this->conexion->prepare($this->sentencia);
+                    $this->PDO->execute();
+                    $this->conexion->commit(); #Consigna una transacción
+                    $this->PDO->setFetchMode(PDO::FETCH_ASSOC);
+                    $this->resultado = $this->PDO->fetchAll(PDO::FETCH_ASSOC);
+                    return $this->resultado;
+                    break;
+                case '1':       #tipo 1 ejecuta un INSERT , UPDATE, DELETE  retorna a true (si no hay falla)
+                    $this->conexion->beginTransaction();
+                    $this->PDO = $this->conexion->prepare($this->sentencia);
+                    $this->PDO->execute($this->datos);
+                    $this->conexion->commit();
+                    return true;
+                    break;
+                default:        # mensaje error si la peticion fue incorrecta
+                    die('[Error 400] => "La Peticion es Incorrecta, solo se permite peticion de tipo 0/1."');
+                    break;
+            }
+        } catch (PDOException $e) { #capturamos el error y se envia la respuesta(ubicacion MODELO)
+            return $this->Capturar_Error($e, "Seguridad"); 
+        }
+    }
+
+    // ===============================================================================
+
+    #sentecias sql en espera de ser llamadas retornan string
+    private function SQL_01():string
+    {
+        return "SELECT * FROM roles_permisos_modulo WHERE rol=:rol";
+    }
+
+    private function SQL_02():string
+    {
+        return "SELECT * FROM personas order by primer_nombre";
+    }
+
+    private function SQL_03():string
+    {
+        return "SELECT  PUM.* , M.nombre FROM permisos_usuario_modulo PUM , modulos M WHERE PUM.cedula_usuario = $cedula AND M.id_modulo = PUM.id_modulo";
+    }
+
+    private function SQL_04():string
+    {
+        return "UPDATE roles_permisos_modulo SET $this->campo = :permiso WHERE rol = :rol AND id_modulo = :id_modulo";
+    }
+
+    private function SQL_05():string
+    {
+        return "UPDATE personas SET rol_inicio=:rol_inicio ,contrasenia = :contrasenia WHERE cedula_persona = :cedula_usuario";
+    }
+
+    private function SQL_06():string
+    {
+        return "UPDATE personas SET estado=:estado  WHERE cedula_persona = :cedula_persona";
+    }
 
     public function get_permisos_rol($rol)
     {
