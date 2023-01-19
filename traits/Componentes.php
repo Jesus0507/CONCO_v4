4a7d98b81d489c90932ecee47aa8ea501305ae18
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2023-01-19 11:02:16 -0400

    ajuste trait

--- traits/Componentes.php
+++ traits/Componentes.php
@@ -13,6 +13,14 @@ trait Componentes
                 'Usuario'    => 'root',
                 'Contraseña' => 'root',
             ),
+            'ByHost' => array(
+                'Servidor'   => 'mysql',
+                'Host'       => 'sql309.byethost12.com',
+                'Base_Datos' => 'epiz_33414627_conco',
+                'Puerto'     => '3306',
+                'Usuario'    => 'b12_32674459',
+                'Contraseña' => 'Cheche482010@',
+            ),
         ];
     }
 
