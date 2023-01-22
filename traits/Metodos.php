4b16a9d05bf5d704fc8981fd17f4ecb5e0cabe2f
Marijozapatav <[marijozapata33@gmail.com]>
2023-01-22 14:31:14 -0400

    Trais Herramientas autocarga de backend

--- traits/Metodos.php
+++ traits/Metodos.php
@@ -1,12 +1,41 @@
 <?php
 
-trait Metodos
+trait Herramientas
 {
+	private $url;
+	private $modulo;
+	private $modelName;
+	private $reflectionClass;
+
+	public $validacion ;
+	public $error;
+
     # VER CONTENIDO DE UN ARRAY DE MANERA ORDENADA
  	public function Ver_Array($value)
     {
         echo "<pre>";
         echo var_dump($value);
         echo "</pre>";
+    }
+
+    # CARGAR ARCHIVOS DE BACKEND
+    public function Validacion($modulo)
+    {
+        $this->url = 'controlador/backend/' . $this->modulo . '_validacion.php';
+
+        if (file_exists($this->url)) {
+            require $this->url;
+
+            $this->modelName       = ucfirst($this->modulo) . '_Validacion';
+            $this->reflectionClass = new ReflectionClass($this->modelName);
+
+            if ($this->reflectionClass->IsInstantiable()) {
+                $this->validacion = new $this->modelName();
+
+            } else {
+                $this->error = '[Error Objeto] => "El Objeto: [ ' . $this->modelName . ' ] No puede ser Instanciado."';
+                return $this->Capturar_Error($this->error);
+            }
+        }
     }
 }
