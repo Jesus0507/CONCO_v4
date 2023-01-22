fb900ef585191d1479f167c076ee7ee24af7e2b5
Marijozapatav <[marijozapata33@gmail.com]>
2023-01-22 19:34:13 -0400

    traits metodos abstract seter de tipos

--- traits/Metodos.php
+++ traits/Metodos.php
@@ -2,8 +2,8 @@
 
 trait Metodos
 {
-	#SETER de sentencias sql mediante codigos
-	abstract public function _SQL_(string $SQL): void();
+	abstract public function _SQL_(string $SQL): void();#SETER de sentencias sql mediante codigos
+	abstract public function _Tipo_(int $tipo): void();#SETER de tipo de ejecucion de modelos
 
 }	
 
