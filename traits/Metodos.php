65f36e5af1cc8694a86a9869839f7445da6b5980
Marijozapatav <[marijozapata33@gmail.com]>
2023-01-22 14:24:44 -0400

    Traits configurarion de metodos ARRAYS

--- traits/Metodos.php
+++ traits/Metodos.php
@@ -2,6 +2,11 @@
 
 trait Metodos
 {
-    
-
+    # VER CONTENIDO DE UN ARRAY DE MANERA ORDENADA
+ 	public function Ver_Array($value)
+    {
+        echo "<pre>";
+        echo var_dump($value);
+        echo "</pre>";
+    }
 }
