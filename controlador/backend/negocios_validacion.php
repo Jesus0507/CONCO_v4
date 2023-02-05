84745c12e55dc68bd589da0a0273d7e81a0abe3c
Josseth Arroyo <cheche482010@gmail.com>
2023-02-05 17:36:07 -0400

    correcion backend negocios

--- controlador/backend/negocios_validacion.php
+++ controlador/backend/negocios_validacion.php
@@ -39,13 +39,13 @@ class Negocios_Validacion extends Validacion
                                 if ($this->Comprobar($this->Datos_Validos()["nombre_negocio"])) {
                                     $this->Errores[] = 'El campo nombre del negocio es obligatorio';
                                 } else {
-                                    if ($this->Validar($this->Datos_Validos()["nombre_negocio"], "caracteres")) {
+                                    if ($this->Validar("caracteres", $this->Datos_Validos()["nombre_negocio"])) {
                                         $this->Errores[] = "El campo nombre del negocio no debe tener caracteres especiales.";
                                     } else {
                                         if ($this->Comprobar($this->Datos_Validos()["cedula_propietario"])) {
                                             $this->Errores[] = 'El campo cedula del propietario es obligatorio';
                                         } else {
-                                            if ($this->Validar($this->Datos_Validos()["cedula_propietario"], "cedula")) {
+                                            if ($this->Validar("cedula",$this->Datos_Validos()["cedula_propietario"])) {
                                                 $this->Errores[] = "La cedula es invalida.";
                                             } else {
                                                 if ($this->Comprobar($this->Datos_Validos()["rif_negocio"])) {
