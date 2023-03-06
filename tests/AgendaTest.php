0d4b3d1e67782521541bef8b802df162cd843b51
Josseth Arroyo <cheche482010@gmail.com>
2023-03-06 12:07:02 -0400

    PRUEBA AGENDA FINALIZADA

--- tests/AgendaTest.php
+++ tests/AgendaTest.php
@@ -7,17 +7,32 @@ class AgendaTest extends TestCase
 {
     private $agenda;
 
-    protected function setUp(): void
-    {
-        $this->agenda = new Agenda_Class();
-    }
+    public $datos = array(
+        'INSERT' => [
+            'tipo_evento' => 'evento de prueba',
+            'fecha'       => '2023-02-10',
+            'creador'     => '7654321',
+            'ubicacion'   => 'ubicacion de prueba',
+            'horas'       => 1,
+            'detalle'     => 'detalle de prueba',
+        ],
+        'UPDATE' => [
+            'id_agenda'   => 1,
+            'tipo_evento' => 'evento actualizado',
+            'fecha'       => '2023-02-11',
+            'creador'     => '7654321',
+            'ubicacion'   => 'ubicacion actualizada',
+            'horas'       => 2,
+            'detalle'     => 'detalle actualizado',
+        ],
+        'DELETE' => ['id_agenda' => 1],  
+    );
 
-    protected function tearDown(): void
-    {
-        $this->agenda = null;
-    }
+    protected function setUp(): void       {$this->agenda = new Agenda_Class();}
 
-    public function test_SQL_01()
+    protected function tearDown(): void    {$this->agenda = null;}
+
+    public function test_SQL_01_SELECT()
     {
         $this->agenda->_SQL_("SQL_01");
         $this->agenda->_Tipo_(0);
@@ -27,42 +42,27 @@ class AgendaTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
-    public function test_SQL_02()
+    public function test_SQL_02_INSERT()
     {
         $this->agenda->_SQL_("SQL_02");
         $this->agenda->_Tipo_(1);
-        $this->agenda->_Datos_([
-            'tipo_evento' => 'evento de prueba',
-            'fecha'       => '2023-02-10',
-            'creador'     => '7654321',
-            'ubicacion'   => 'ubicacion de prueba',
-            'horas'       => 1,
-            'detalle'     => 'detalle de prueba',
-        ]);
+        $this->agenda->_Datos_($this->datos["INSERT"]);
         $result = $this->agenda->Administrar();
 
         $this->assertTrue($result);
     }
 
-    public function test_SQL_03()
+    public function test_SQL_03_UPDATE()
     {
         $this->agenda->_SQL_("SQL_03");
         $this->agenda->_Tipo_(1);
-        $this->agenda->_Datos_([
-            'id_agenda'   => 1,
-            'tipo_evento' => 'evento actualizado',
-            'fecha'       => '2023-02-11',
-            'creador'     => '7654321',
-            'ubicacion'   => 'ubicacion actualizada',
-            'horas'       => 2,
-            'detalle'     => 'detalle actualizado',
-        ]);
+        $this->agenda->_Datos_($this->datos["UPDATE"]);
         $result = $this->agenda->Administrar();
 
         $this->assertTrue($result);
     }
 
-    public function test_SQL_04()
+    public function test_SQL_04_SELECT()
     {
         $this->agenda->_SQL_("SQL_04");
         $this->agenda->_Tipo_(0);
@@ -72,7 +72,7 @@ class AgendaTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
-    public function test_SQL_05()
+    public function test_SQL_05_SELECT_WHERE()
     {
         $this->agenda->_SQL_("SQL_05");
         $this->agenda->_Tipo_(0);
@@ -82,11 +82,11 @@ class AgendaTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
-    public function test_SQL_06()
+    public function test_SQL_06_DELETE()
     {
         $this->agenda->_SQL_("SQL_06");
         $this->agenda->_Tipo_(1);
-        $this->agenda->_Datos_(['id_agenda' => 1]);
+        $this->agenda->_Datos_($this->datos["DELETE"]);
         $result = $this->agenda->Administrar();
 
         $this->assertTrue($result);
