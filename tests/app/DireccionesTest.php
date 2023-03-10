eedf10bd625f17bd0321a7a41f6c3b9e6b92964e
Josseth Arroyo <cheche482010@gmail.com>
2023-03-10 16:23:55 -0400

    PRUEBAS OTRAS 2
    PRUEBA DE DIRECCIONES URL

--- tests/app/DireccionesTest.php
+++ tests/app/DireccionesTest.php
@@ -1,47 +1,59 @@
 <?php
 
-require_once 'Direcciones.php';
-
 use PHPUnit\Framework\TestCase;
 
 class DireccionesTest extends TestCase
 {
-    public function testSeguridadCodificar()
+
+    protected function setUp(): void
+    {$this->direcciones = new Direcciones();}
+
+    protected function tearDown(): void
+    {$this->direcciones = null;}
+
+    public function test_SeguridadCodificar()
     {
-        $entrada  = 'https://www.google.com';
+        $entrada  = 'https://127.0.0.1/CONCO_V4';
         $esperado = Direcciones::Seguridad($entrada, 'codificar');
-
         $this->assertIsString($esperado);
-        $this->assertNotEquals($entrada, $esperado);
+        $this->assertNotEquals($entrada, Direcciones::Seguridad($entrada, 'decodificar'));
     }
 
-    public function testSeguridadDecodificar()
+    public function test_Seguridad_Decodificar()
     {
-        $entrada  = 'L2ZvcnVtL2luZGV4LnBocA==';
+        $entrada  = 'R3laSFNoNlpqanNUMlpMZDh4MzYzQT09';
         $esperado = Direcciones::Seguridad($entrada, 'decodificar');
-
         $this->assertIsString($esperado);
-        $this->assertEquals('http://formulacion.php', $esperado);
+        $this->assertEquals("Inicio/", $esperado);
     }
 
-    public function testURL()
+    public function test_Base_URL()
     {
         $esperado = Direcciones::URL();
-
         $this->assertIsString($esperado);
         $this->assertNotEmpty($esperado);
     }
 
     public function test_000_()
     {
-        $entrada  = 'https://www.google.com';
-        $esperado = Direcciones::Seguridad($entrada, 'codificar');
-
         ob_start();
-        Direcciones::_000_($esperado);
-        $salida = ob_get_clean();
+        Direcciones::_000_('Reportes/Administrar/');
+        $salida   = ob_get_clean();
+        $esperado = URL . Direcciones::Seguridad('Reportes/Administrar/', 'codificar');
+        $this->assertEquals($esperado, $salida);
+        $this->assertNotEmpty($esperado);
+    }
 
-        $this->expectOutputString(Direcciones::URL() . $esperado);
-        $this->assertEquals($salida, Direcciones::URL() . $esperado);
+    public function test_Direcciones_001_045()
+    {
+        
+        for ($i = 1; $i <= 45; $i++) {
+            $nombreFuncion = '_' . str_pad($i, 3, "0", STR_PAD_LEFT) . '_';
+            ob_start();
+            Direcciones::$nombreFuncion();
+            $esperado = ob_get_clean();
+            $this->assertIsString($esperado);
+            $this->assertNotEmpty($esperado);
+        }
     }
 }
