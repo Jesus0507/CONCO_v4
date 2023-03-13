98ccc6cc682f2538332ffecc07b0fddf608fe849
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 14:04:15 -0400

    PRUEBA CONTROLADOR DISCAPACITADO FINALAZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,17 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlado Consejo Comunal =================== 
+echo ===================Pruebas del Controlador Discapacitados =================== 
 echo.
-php vendor/bin/phpunit tests/Consejo_ComunalTestControlador.php 
---testdox-xml tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.xml 
---testdox-html tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.html 
---testdox 
---log-junit tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.xml  
---testdox 
---log-junit tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.log  
---cache-result-file tests/cache/.phpunit.consejo_comunal.controlador.result.cache 
---debug -v
+php vendor/bin/phpunit tests/DiscapacitadosTestControlador.php --testdox-xml tests/resultados/discapacitados/discapacitados-controlador-resultados.xml --testdox-html tests/resultados/discapacitados/discapacitados-controlador-resultados.html --testdox --log-junit tests/resultados/discapacitados/discapacitados-controlador-resultados.xml  --testdox --log-junit tests/resultados/discapacitados/discapacitados-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.discapacitados.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
