18127ac554d62a03c866c5447569b08ce16d4dd4
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 16:51:59 -0400

    PRUEBA CONTROLADOR PERONAS  FINALIZADO

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador rto_Humanizado =================== 
+echo ===================Pruebas del Controlador Personas =================== 
 echo.
-php vendor/bin/phpunit tests/Parto_HumanizadoTestControlador.php --testdox-xml tests/resultados/parto_humanizado/parto_humanizado-controlador-resultados.xml --testdox-html tests/resultados/parto_humanizado/parto_humanizado-controlador-resultados.html --testdox --log-junit tests/resultados/parto_humanizado/parto_humanizado-controlador-resultados.xml  --testdox --log-junit tests/resultados/parto_humanizado/parto_humanizado-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.parto_humanizado.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/PersonasTestControlador.php --testdox-xml tests/resultados/personas/personas-controlador-resultados.xml --testdox-html tests/resultados/personas/personas-controlador-resultados.html --testdox --log-junit tests/resultados/personas/personas-controlador-resultados.xml  --testdox --log-junit tests/resultados/personas/personas-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.personas.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
