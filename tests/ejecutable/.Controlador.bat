099fe7384763cd4bd1498eca8b3ba12fcc0d5940
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 14:35:40 -0400

    PRUEBA CONTROLADOR FINALIZADO

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,17 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Enfermos =================== 
+echo ===================Pruebas del Controlador Familias =================== 
 echo.
-php vendor/bin/phpunit tests/EnfermosTestControlador.php --testdox-xml tests/resultados/enfermos/enfermos-controlador-resultados.xml --testdox-html tests/resultados/enfermos/enfermos-controlador-resultados.html --testdox --log-junit tests/resultados/enfermos/enfermos-controlador-resultados.xml  --testdox --log-junit tests/resultados/enfermos/enfermos-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.enfermos.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/FamiliasTestControlador.php 
+--testdox-xml tests/resultados/familias/familias-controlador-resultados.xml 
+--testdox-html tests/resultados/familias/familias-controlador-resultados.html 
+--testdox 
+--log-junit tests/resultados/familias/familias-controlador-resultados.xml  
+--testdox 
+--log-junit tests/resultados/familias/familias-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.familias.controlador.result.cache 
+--debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
