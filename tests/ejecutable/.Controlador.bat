c303230861ff3a6be51ec46f9ad52bdfa3d20fad
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 17:54:51 -0400

    PRUEBA CONTROLADOR VIVIENDA FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,17 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Vacunados =================== 
+echo ===================Pruebas del Controlador Viviendas =================== 
 echo.
-php vendor/bin/phpunit tests/VacunadosTestControlador.php --testdox-xml tests/resultados/vacunados/vacunados-controlador-resultados.xml --testdox-html tests/resultados/vacunados/vacunados-controlador-resultados.html --testdox --log-junit tests/resultados/vacunados/vacunados-controlador-resultados.xml  --testdox --log-junit tests/resultados/vacunados/vacunados-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.vacunados.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/ViviendasTestControlador.php 
+--testdox-xml tests/resultados/vivienda/vivienda-controlador-resultados.xml 
+--testdox-html tests/resultados/vivienda/vivienda-controlador-resultados.html 
+--testdox 
+--log-junit tests/resultados/vivienda/vivienda-controlador-resultados.xml  
+--testdox 
+--log-junit tests/resultados/vivienda/vivienda-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.vivienda.controlador.result.cache 
+--debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
