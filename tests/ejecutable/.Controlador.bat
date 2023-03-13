d12964f3af453ee51844853a3d9b3ae4395e8856
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 17:11:25 -0400

    PRUEBA CONTROLADOR SECTOR AGRICOLA FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Reportes =================== 
+echo ===================Pruebas del Controlador Secton Agricola =================== 
 echo.
-php vendor/bin/phpunit tests/ReportesTestControlador.php --testdox-xml tests/resultados/reportes/reportes-controlador-resultados.xml --testdox-html tests/resultados/reportes/reportes-controlador-resultados.html --testdox --log-junit tests/resultados/reportes/reportes-controlador-resultados.xml  --testdox --log-junit tests/resultados/reportes/reportes-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.reportes.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/Sector_AgricolaTestControlador.php --testdox-xml tests/resultados/sector_agricola/sector_agricola-controlador-resultados.xml --testdox-html tests/resultados/sector_agricola/sector_agricola-controlador-resultados.html --testdox --log-junit tests/resultados/sector_agricola/sector_agricola-controlador-resultados.xml  --testdox --log-junit tests/resultados/sector_agricola/sector_agricola-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.sector_agricola.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
