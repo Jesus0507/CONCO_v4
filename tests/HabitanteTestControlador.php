55be591d11002e61672f9ae0da8d481b6b36c413
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 15:47:20 -0400

    PRUEBA CONTROLADOR HABITANTE FINALIZADA

---
+++ tests/HabitanteTestControlador.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class HabitanteTestControlador extends TestCase
+{
+    private $vista;
+
+    protected function setUp(): void
+    {
+        $this->vista = new Vista();
+    }
+
+    protected function tearDown(): void
+    {$this->Agenda = null;}
+
+    // AGENDA
+    public function test_Cargar_Vistas()
+    {
+        ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Establecer_Consultas()
+    {
+        ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Getters()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    // =================================================================
+}
