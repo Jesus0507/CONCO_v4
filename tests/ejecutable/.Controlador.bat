3a288ecb03c36559acff6394a59208e1697a278b
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 17:34:52 -0400

    PRUEBA CONTROLADOR SOLICITUDES FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,17 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Seguridad =================== 
+echo ===================Pruebas del Controlador Solicitudes =================== 
 echo.
-php vendor/bin/phpunit tests/SeguridadTestControlador.php --testdox-xml tests/resultados/seguridad/seguridad-controlador-resultados.xml --testdox-html tests/resultados/seguridad/seguridad-controlador-resultados.html --testdox --log-junit tests/resultados/seguridad/seguridad-controlador-resultados.xml  --testdox --log-junit tests/resultados/seguridad/seguridad-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.seguridad.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/SolicitudesTestControlador.php 
+--testdox-xml tests/resultados/solicitudes/solicitudes-controlador-resultados.xml 
+--testdox-html tests/resultados/solicitudes/solicitudes-controlador-resultados.html 
+--testdox 
+--log-junit tests/resultados/solicitudes/solicitudes-controlador-resultados.xml  
+--testdox 
+--log-junit tests/resultados/solicitudes/solicitudes-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.solicitudes.controlador.result.cache 
+--debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
