3554601f1b0adc67e15dfcdcb4f23045a01c7624
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 13:08:28 -0400

    PRUEBA CONTROLADOR CENTRO VOTACION FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,16 +2,16 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlado Bitacora=================== 
+echo ===================Pruebas del Controlado Centro Votacion=================== 
 echo.
-php vendor/bin/phpunit tests/BitacoraTestControlador.php 
---testdox-xml tests/resultados/bitacora/bitacora-controlador-resultados.xml 
---testdox-html tests/resultados/bitacora/bitacora-controlador-resultados.html 
+php vendor/bin/phpunit tests/Centro_VotacionTestControlador.php 
+--testdox-xml tests/resultados/centro_votacion/centro_votacion-controlador-resultados.xml 
+--testdox-html tests/resultados/centro_votacion/centro_votacion-controlador-resultados.html 
 --testdox 
---log-junit tests/resultados/bitacora/bitacora-controlador-resultados.xml  
+--log-junit tests/resultados/centro_votacion/centro_votacion-controlador-resultados.xml  
 --testdox 
---log-junit tests/resultados/bitacora/bitacora-controlador-resultados.log  
---cache-result-file tests/cache/.phpunit.bitacora.controlador.resultcache 
+--log-junit tests/resultados/centro_votacion/centro_votacion-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.centro_votacion.controlador.resultcache 
 --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
