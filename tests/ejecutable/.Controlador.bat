3790e120d47eceee059691548d15822bf8eccee5
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 14:23:54 -0400

    PRUEBAS CONTROLADOR ENFERMOS FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Discapacitados =================== 
+echo ===================Pruebas del Controlador Enfermos =================== 
 echo.
-php vendor/bin/phpunit tests/DiscapacitadosTestControlador.php --testdox-xml tests/resultados/discapacitados/discapacitados-controlador-resultados.xml --testdox-html tests/resultados/discapacitados/discapacitados-controlador-resultados.html --testdox --log-junit tests/resultados/discapacitados/discapacitados-controlador-resultados.xml  --testdox --log-junit tests/resultados/discapacitados/discapacitados-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.discapacitados.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/EnfermosTestControlador.php --testdox-xml tests/resultados/enfermos/enfermos-controlador-resultados.xml --testdox-html tests/resultados/enfermos/enfermos-controlador-resultados.html --testdox --log-junit tests/resultados/enfermos/enfermos-controlador-resultados.xml  --testdox --log-junit tests/resultados/enfermos/enfermos-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.enfermos.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
