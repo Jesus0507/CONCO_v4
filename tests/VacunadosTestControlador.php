6f1b2d4db427a7204b331b994f98c8fcb76f7fdf
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 17:43:54 -0400

    PRUEBA CONTROLADOR VACUNADOS FINALIZADA

---
+++ tests/VacunadosTestControlador.php
@@ -0,0 +1,83 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class VacunadosTestControlador extends TestCase
+{
+    private $vista;
+
+    protected function setUp(): void
+    {
+        $this->vista = new Vista();
+    }
+
+    protected function tearDown(): void
+    {$this->Agenda = null;}
+
+    // AGENDA
+    public function test_Cargar_Vistas()
+    {
+        ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Establecer_Consultas()
+    {
+        ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Getters()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Administrar_Vistas_Consultas_Registros()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Administrar_Registrar()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Administrar_Consulta_Ajax()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Administrar_Editar()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+
+    public function test_Administrar_Eliminar()
+    {
+       ob_start();
+        $this->vista->Cargar_Vistas('inicio/index');
+        $contenido_vista = ob_get_clean();
+        $this->assertNotEmpty($contenido_vista);
+    }
+    // =================================================================
+}
--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,17 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Solicitudes =================== 
+echo ===================Pruebas del Controlador Vacunados =================== 
 echo.
-php vendor/bin/phpunit tests/SolicitudesTestControlador.php 
---testdox-xml tests/resultados/solicitudes/solicitudes-controlador-resultados.xml 
---testdox-html tests/resultados/solicitudes/solicitudes-controlador-resultados.html 
---testdox 
---log-junit tests/resultados/solicitudes/solicitudes-controlador-resultados.xml  
---testdox 
---log-junit tests/resultados/solicitudes/solicitudes-controlador-resultados.log  
---cache-result-file tests/cache/.phpunit.solicitudes.controlador.result.cache 
---debug -v
+php vendor/bin/phpunit tests/VacunadosTestControlador.php --testdox-xml tests/resultados/vacunados/vacunados-controlador-resultados.xml --testdox-html tests/resultados/vacunados/vacunados-controlador-resultados.html --testdox --log-junit tests/resultados/vacunados/vacunados-controlador-resultados.xml  --testdox --log-junit tests/resultados/vacunados/vacunados-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.vacunados.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
