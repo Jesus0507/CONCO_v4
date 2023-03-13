26d93e0e41f24139fae6c979f91b937a170fd9a6
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 13:37:14 -0400

    PRUEBAS CONSEJO COMUNAL FINALIZADO

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,16 +2,16 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlado Centro Votacion=================== 
+echo ===================Pruebas del Controlado Consejo Comunal =================== 
 echo.
-php vendor/bin/phpunit tests/Centro_VotacionTestControlador.php 
---testdox-xml tests/resultados/centro_votacion/centro_votacion-controlador-resultados.xml 
---testdox-html tests/resultados/centro_votacion/centro_votacion-controlador-resultados.html 
+php vendor/bin/phpunit tests/Consejo_ComunalTestControlador.php 
+--testdox-xml tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.xml 
+--testdox-html tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.html 
 --testdox 
---log-junit tests/resultados/centro_votacion/centro_votacion-controlador-resultados.xml  
+--log-junit tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.xml  
 --testdox 
---log-junit tests/resultados/centro_votacion/centro_votacion-controlador-resultados.log  
---cache-result-file tests/cache/.phpunit.centro_votacion.controlador.resultcache 
+--log-junit tests/resultados/consejo_comunal/consejo_comunal-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.consejo_comunal.controlador.result.cache 
 --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
