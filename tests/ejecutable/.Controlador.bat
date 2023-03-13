c8b55932a899647dbc7fceace635a5d8dfbafed7
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 16:21:21 -0400

    PRUEBAS CONTROLADOR NEGOCIOS FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,9 +2,17 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Login =================== 
+echo ===================Pruebas del Controlador Negocios =================== 
 echo.
-php vendor/bin/phpunit tests/LoginTestControlador.php --testdox-xml tests/resultados/login/login-controlador-resultados.xml --testdox-html tests/resultados/login/login-controlador-resultados.html --testdox --log-junit tests/resultados/login/login-controlador-resultados.xml  --testdox --log-junit tests/resultados/login/login-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.login.controlador.result.cache --debug -v
+php vendor/bin/phpunit tests/NegociosTestControlador.php 
+--testdox-xml tests/resultados/negocios/negocios-controlador-resultados.xml 
+--testdox-html tests/resultados/negocios/negocios-controlador-resultados.html 
+--testdox 
+--log-junit tests/resultados/negocios/negocios-controlador-resultados.xml  
+--testdox 
+--log-junit tests/resultados/negocios/negocios-controlador-resultados.log  
+--cache-result-file tests/cache/.phpunit.negocios.controlador.result.cache 
+--debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
