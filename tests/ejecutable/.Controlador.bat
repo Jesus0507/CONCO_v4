a0b1eb1cf7b1a7f29a36c737f954e71976b82d9d
Josseth Arroyo <cheche482010@gmail.com>
2023-03-13 16:03:39 -0400

    PREUBA CONTROLADRO INMUEBLE FINALIZADA

--- tests/ejecutable/.Controlador.bat
+++ tests/ejecutable/.Controlador.bat
@@ -2,17 +2,9 @@
 cd /
 cd C:\xampp\htdocs\dashboard\www\CONCO_v4
 echo.
-echo ===================Pruebas del Controlador Habitante =================== 
+echo ===================Pruebas del Controlador Inmuebles =================== 
 echo.
-php vendor/bin/phpunit tests/HabitanteTestControlador.php 
---testdox-xml tests/resultados/habitante/habitante-controlador-resultados.xml 
---testdox-html tests/resultados/habitante/habitante-controlador-resultados.html 
---testdox 
---log-junit tests/resultados/habitante/habitante-controlador-resultados.xml  
---testdox 
---log-junit tests/resultados/habitante/habitante-controlador-resultados.log  
---cache-result-file tests/cache/.phpunit.habitante.controlador.result.cache 
---debug -v
+php vendor/bin/phpunit tests/InmueblesTestControlador.php --testdox-xml tests/resultados/inmuebles/inmuebles-controlador-resultados.xml --testdox-html tests/resultados/inmuebles/inmuebles-controlador-resultados.html --testdox --log-junit tests/resultados/inmuebles/inmuebles-controlador-resultados.xml  --testdox --log-junit tests/resultados/inmuebles/inmuebles-controlador-resultados.log  --cache-result-file tests/cache/.phpunit.inmuebles.controlador.result.cache --debug -v
 echo.
 echo ======================Fin de las Pruebas======================
 echo.
